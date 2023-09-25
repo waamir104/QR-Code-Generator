a11f3dc75becf182cc9f0b4ba6b60af6238fcd86
waamir104 <waamirdev@gmail.com>
2023-09-24 23:12:16 -0500

    Developing the QR generator and share button functionalities

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+const qr_container = document.getElementById("qr_container");
+const download_btn = document.getElementById("download_btn");
+const share_btn = document.getElementById("share_btn");
+
+var searchString = window.location.search;
+var searchParams = new URLSearchParams(searchString);
+var url = searchParams.get("url");
+
+var qrcode = new QRCode(qr_container, {
+	text: url,
+	width: 180,
+	height: 180,
+	colorDark : "#000000",
+	colorLight : "#ffffff",
+	correctLevel : QRCode.CorrectLevel.H
+});
+
+download_btn.addEventListener("click", () => {
+	// TODO
+})
+
+share_btn.addEventListener("click", () => {
+	navigator.clipboard.writeText(url);
+	copy__btn__alert();
+})
+
+function copy__btn__alert() {
+    Swal.fire({
+        width: '30%',
+        icon: 'success',
+        title: 'URL Copied',
+        showConfirmButton: false,
+        timer: 1700,
+        backdrop: true,
+        timerProgressBar: true,
+        position: 'center',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+        padding: '0 0 3em 0'
+    });
+}
